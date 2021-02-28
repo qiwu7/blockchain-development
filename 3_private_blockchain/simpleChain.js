f62669d2a397d291558f3f79fa7cfcfe94f250f0
Qi Wu <qiwu@alumni.stanford.edu>
2021-02-27 21:27:21 -0800

    Add block class

---
+++ 3_private_blockchain/simpleChain.js
@@ -0,0 +1,13 @@
+/* ===== Block Class ===================================
+|  Class with a constructor for block data model       |
+|  ====================================================*/
+
+class Block {
+    constructor(data) {
+        this.height = '';
+        this.timeStamp = '';
+        this.data = data;
+        this.previousHash = '0x';
+        this.hash = '';
+    }
+}
