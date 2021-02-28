db188f26510679cd91e55a851c76f80ede7c83df
Qi Wu <qiwu@alumni.stanford.edu>
2021-02-27 21:41:36 -0800

    Store blocks

--- 3_private_blockchain/simpleChain.js
+++ 3_private_blockchain/simpleChain.js
@@ -1,3 +1,5 @@
+import SHA256 from 'crypto-js/sha256';
+
 /* ===== Block Class ===================================
 |  Class with a constructor for block data model       |
 |  ====================================================*/
@@ -9,5 +11,39 @@ class Block {
         this.data = data;
         this.previousHash = '0x';
         this.hash = '';
+    }
+}
+
+/* ===== Blockchain ===================================
+|  Class with a constructor for blockchain data model  |
+|  with functions to support:                          |
+|     - createGenesisBlock()                           |
+|     - getLatestBlock()                               |
+|     - addBlock()                                     |
+|     - getBlock()                                     |
+|     - validateBlock()                                |
+|     - validateChain()                                |
+|  ====================================================*/
+
+class Blockchain {
+    constructor() {
+        // new chain array
+        this.chain = [];
+        this.addBlock(this.genesisBlock())
+    }
+
+    genesisBlock() {
+        return new Block("First block in the chain - Genesis block");
+    }
+
+    // addBlock method
+    addBlock(newBlock) {
+        if (this.chain.length > 0) {
+            // previous block hash
+            newBlock.previousHash = this.chain[this.chain.length - 1].hash;
+        }
+        // SHA256 requires a string of data
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+        this.chain.push(newBlock);
     }
 }
